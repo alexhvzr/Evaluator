b99be7b498ef34a7b676a50177b50aebed763b87
Alex Hayden van Zuiden-Rylander <alex@D-10-156-2-100.dhcp4.washington.edu>
2019-03-04 09:38:52 -0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Evaluator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Evaluator.dir/main.cpp.o"
+  "Evaluator.pdb"
+  "Evaluator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Evaluator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
